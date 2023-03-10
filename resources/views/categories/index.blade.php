ad528d94c55f52df8ae487eb1ac618dc5f309ee5
Aarti Chauhan <aarti.c@biztechcs.com>
2023-03-10 22:12:30 +0530

    init

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('content')
+<div class="pull-right">
+        <a class="btn btn-primary" href="{{ route(categories.create  ) }}"> Add</a>
+</div>
+TESTTT Demo
+@endsection
